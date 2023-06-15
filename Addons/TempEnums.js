fab1adc2242009f2d0126759f18c93cd8b13ff9b
Rafski <page+github@kalinowski.app>
2023-06-15 19:18:55 +0200

    15.6.23

--- Addons/TempEnums.js
+++ Addons/TempEnums.js
@@ -3,9 +3,9 @@ const LaezariaEnums = Object.freeze({
     APPROVED_URL: 'https://i.imgur.com/DQwdu0u.png',
 
     FAILED_PROOF_URL: 'https://i.imgur.com/ReMlAhB.png',
-    INBOX_CHANNEL_ID: '1103770722586853518',
+    INBOX_CHANNEL_ID: '1099183284115755008',
 
-    MEMBER_ROLE_ID: '1099703236983791698',
+    MEMBER_ROLE_ID: '259895578421362693',
 });
 
 const NorthEnums = Object.freeze({
